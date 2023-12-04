fd984c667601e4db2242c4ba9955c05cb5568286
ibtissammassa <bettysama16@gmail.com>
2023-12-04 13:42:12 +0100

    certificate

--- reactapp/src/pages/Certificate.jsx
+++ reactapp/src/pages/Certificate.jsx
@@ -1,8 +1,10 @@
 
 function Certificate() {
   return (
-    <div className="certificate">
-      <h1>Certificate</h1>
+      <div className="container">
+          <h2 className="subtitle">Congratulations!</h2>
+          <h1>You've Earned Your Certificate</h1>
+          <p>Thank you for completing the course. You have successfully completed the course and earned your certificate. Your dedication and hard work have paid off. Download your certificate to showcase your achievement.</p>
     </div>
   );
 }

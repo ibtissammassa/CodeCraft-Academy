0d0cf1dd37cc673809a1274d003fea0f86c2d5da
ibtissammassa <bettysama16@gmail.com>
2023-12-04 14:16:39 +0100

    meta data + separate css files

--- reactapp/src/Components/Footer.jsx
+++ reactapp/src/Components/Footer.jsx
@@ -1,68 +1,32 @@
-
+import React from 'react';
+import '../assets/css/Footer.css'; // Import the CSS file
 
 function Footer() {
-    const barStyles = {
-        width: '100%',
-        height: '40px',
-        background: 'var(--primary-color, #157AFE)',
-        boxShadow: '0px 2px 6px 0px rgba(0, 0, 0, 0.25)',
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'center',
-    }
-
-    const headingStyles = {
-        color: 'var(--primary-color)',
-        fontSize: '17px',
-        marginBottom: '14px',
-    }
-
-    const containerStyles = {
-        position: 'absolute',
-        top: '100%',
-        borderTop: '1px solid #E5E5E5',
-        zIndex: '10',
-    }
-
-    const mainStyles = {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        padding: '30px 45px'
-    }
-
-    const aboutStyles = {
-        width: '50%',
-    }
-
-    const liStyles = {
-        listStyle: 'none',
-    }
     return (
-        <div style={containerStyles}>
-            <div style={mainStyles}>
-                <div style={aboutStyles}>
-                    <h3 style={headingStyles}>About Us</h3>
+        <div className="footer-container">
+            <div className="main">
+                <div className="about">
+                    <h3 className="heading">About Us</h3>
                     <p>Welcome to E-learning, your gateway to comprehensive tech education. We are passionate about fostering a community-driven learning environment where individuals from all backgrounds can expand their knowledge horizons. With an array of meticulously crafted courses and a commitment to providing cutting-edge content, we aim to equip you with the skills needed to thrive in today's digital landscape. Let's embark on this educational journey together and unlock your potential!</p>
                 </div>
                 <div>
-                    <h3 style={headingStyles}>Quick Links</h3>
-                    <ul>
-                        <li style={liStyles}>Home</li>
-                        <li style={liStyles}>Course</li>
-                        <li style={liStyles}>Blog</li>
-                        <li style={liStyles}>Contact</li>
+                    <h3 className="heading">Quick Links</h3>
+                    <ul className="links">
+                        <li>Home</li>
+                        <li>Course</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
                     </ul>
                 </div>
                 <div>
-                    <h3 style={headingStyles}>Connect With Us</h3>
-                    <ul>
-                        <li style={liStyles}>info@yourcompany.example.com</li>
-                        <li style={liStyles}>+1(450) 999-1111</li>
+                    <h3 className="heading">Connect With Us</h3>
+                    <ul className="contact">
+                        <li>info@yourcompany.example.com</li>
+                        <li>+1(450) 999-1111</li>
                     </ul>
                 </div>
             </div>
-            <div style={barStyles}>
+            <div className="bar">
                 <p>&copy; 2023 E-Learning All rights reserved.</p>
             </div>
         </div>
--- reactapp/src/Components/NavBar.jsx
+++ reactapp/src/Components/NavBar.jsx
@@ -1,40 +1,22 @@
-import logo from '../assets/E-learning.svg'
+import logo from '../assets/E-learning.svg';
 import SearchBar from './UI/SearchBar';
-import {Link } from "react-router-dom"; 
+import { Link } from 'react-router-dom';
+import '../assets/css/Navbar.css'; // Import the CSS file
 
 function Navbar() {
-    const containerStyles = {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-    };
-
-    const topBarStyles = {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        padding: '20px 35px'
-    }
-
-    const barStyles = {
-        width: '100%',
-        height:'30px',
-        background: 'var(--primary-color, #157AFE)',
-        boxShadow: '0px 2px 6px 0px rgba(0, 0, 0, 0.25)'
-    }
-
-    const logoContainerStyles = {
-        display: 'flex',
-        alignItems: 'center'
-       
-    }
     return (
-        <div style={containerStyles}>
-            <div style={topBarStyles}>
-                <Link style={logoContainerStyles} to="/"><img src={logo} alt="Logo" /></Link>
+        <div className="navbar-container">
+            <div className="top-bar">
+                <Link to="/" className="logo-container">
+                    <img src={logo} alt="Logo" />
+                </Link>
                 <SearchBar />
-            </div>           
-            <div style={barStyles}></div>
+            </div>
+            <div className="bar">
+                <p>Home</p>
+                <p>My Courses</p>
+                <p>Contact Us</p>
+            </div>
         </div>
     );
 }
